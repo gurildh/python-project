80440fe3589faaafb6780dffc0efddafd769ae6a
gurildh <41983365+gurildh@users.noreply.github.com>
2018-08-06 02:58:27 -0400

    Add files via upload

---
+++ BUS.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter.ttk as ttk
+import random
+import time
+import datetime
+
+root=Tk()
+root.geometry("1400x650")
+root.configure(background="black")
+
+f1=Frame(root, width=550, height=650,bd=8,relief="raise")
+f1.pack(side=LEFT, fill=Y)
+f2=Frame(root, width=850, height=650,bd=8,relief="raise")
+f2.pack(side=RIGHT, fill=Y)
+
+
+
+
+
+
+
+
+
+root.mainloop()
